b5a80b2cadf0ad524988495cf64fe0dde5a584a6
Thomas Andrews <github@thomasoandrews.com>
2023-05-09 10:58:07 -0400

    Add UpcaseMap for simple lookups.

---
+++ src/generic/maps.ts
@@ -0,0 +1,28 @@
+export class TransformKeyMap<K1,K2,V> {
+    map:Map<K2,V> 
+    transform: (key:K1) => K2
+
+    constructor(transform: (key:K1)=> K2, map:Map<K2,V> = new Map<K2,V>()) {
+        this.transform = transform
+        this.map = map
+    }
+
+    get(k1:K1):V|undefined {
+       return this.map.get(this.transform(k1))
+    }
+
+    set(k1:K1,value: V) {
+       this.map.set(this.transform(k1),value)
+    }
+
+    has(key:K1):boolean {
+        return this.map.has(this.transform(key))
+    }
+
+}
+
+export class UpcaseMap<V> extends TransformKeyMap<String,String,V> {
+   constructor() {
+       super((s:String) => s.toUpperCase())
+   }
+}
