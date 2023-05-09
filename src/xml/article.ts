716947bf4372b2b1374e2ab3f789f0712c32400f
Thomas Andrews <github@thomasoandrews.com>
2023-05-09 11:38:59 -0400

    First pass at article parser.

---
+++ src/xml/article.ts
@@ -0,0 +1,42 @@
+import {Deck, Seats, Suit, Rank, Card, Seat} from '../bridge/constants'
+
+interface ParseContext {
+    inParagraph: Boolean
+    addCard(c:Card):void 
+    addHolding(ranks: Rank[], suit: Suit|undefined):void
+}
+
+type ElementCallback = (e: Element, ctx: ParseContext) => void
+
+class ArticleParser {
+    readonly elementMap = new Map<string,ElementCallback>()
+
+    determineHolding(element:Element, ctx:ParseContext):string {    
+        const cards = element.getAttribute('cards')
+        if (cards) {
+            const ranks = Deck.ranks.parse(cards)
+            return ranks.map((rank) => rank.brief).join("-")
+        }
+    
+        return '-'
+    }
+    
+    suitCallback(suit:Suit, element:Element, ctx:ParseContext):string {
+        const card = element.getAttribute('card')
+        if (card) {
+            const rank = Deck.ranks.byName(card)
+            ctx.addCard(rank.of(suit))
+        }
+
+        return suit.symbol + ' '+ this.determineHolding(element,ctx)
+    }
+
+    constructor() {
+        const _this = this
+        Deck.suits.each ((suit:Suit) => {
+            this.elementMap.set(suit.singular,(e:Element, ctx:ParseContext) => { 
+                _this.suitCallback(suit,e,ctx)
+            })
+        })
+    }
+}
